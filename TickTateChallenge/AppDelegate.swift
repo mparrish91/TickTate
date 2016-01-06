4860177b72d2685304282674157c0f9091e400c6
malcolm <mparrish91@gmail.com>
2016-01-06 14:17:46 -0800

    first commit

--- TickTateChallenge/AppDelegate.swift
+++ TickTateChallenge/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("mut3gKF3XXSzmPXtMjepzCGzZSp0Ktk1ze6dN1s5",
+            clientKey: "A9Mn3FWaxJhrRvT1xDNUlXNZatd3qAHQi2xiQMWb")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        
         return true
     }
 
